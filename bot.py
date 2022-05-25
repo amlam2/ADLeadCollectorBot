4663ec5911aea5f9e49999372797f08f067c7460
Alen Labovich <amlam2@yandex.by>
2022-05-25 16:51:48 +0300

    first test

--- bot.py
+++ bot.py
@@ -1,4 +1,10 @@
 import config
 import telebot
 
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, '<b>Привет!</b>', parse_mode='html')
+
+bot.polling(none_stop=True)
